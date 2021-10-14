0a8266520989cd2bd062236bce5923ef2d4452b5
atakanteko <atakantekoglu@gmail.com>
2021-10-14 23:53:31 +0300

    heroku

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+require('dotenv').config()
+const mongoose = require('mongoose')
+const Todo = require('./models/todo')
 
 app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
+
+
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -16,72 +21,35 @@ const requestLogger = (request, response, next) => {
 
 app.use(requestLogger)
 
-let todos = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2020-01-10T17:30:31.098Z",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2020-01-10T18:39:34.091Z",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2020-01-10T19:20:14.298Z",
-    important: true
-  }
-]
-
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
-})
 
 app.get('/api/todos', (req, res) => {
-  res.json(todos)
+  Todo.find({}).then(result => {
+      res.json(result)
+      })
 })
 
-const generateId = () => {
-  const maxId = todos.length > 0
-    ? Math.max(...todos.map(n => n.id))
-    : 0
-  return maxId + 1
-}
 
 app.post('/api/todos', (request, response) => {
   const body = request.body
 
-  if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
   }
 
-  const todo = {
-    content: body.content,
-    important: body.important || false,
-    date: new Date(),
-    id: generateId(),
-  }
 
-  todos = todos.concat(todo)
+  const todo = new Todo({
+    content: body.content
+  })
 
-  response.json(todo)
+  todo.save().then(savedTodo => {
+    response.json(savedTodo)
+  })
 })
 
 app.get('/api/todos/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const todo = todos.find(todo => todo.id === id)
-
-  if (todo) {
-    response.json(todo)
-  } else {
-    response.status(404).end()
-  }
+  Todo.findById(request.params.id).then(t => {
+    response.json(t)
+  })
 })
 
 app.delete('/api/todos/:id', (request, response) => {
@@ -97,7 +65,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
